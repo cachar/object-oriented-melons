c744a98a06f1b7d33827eee23f7ea9aa120c5493
Hackbright Student <info@hackbrightacademy.com>
2016-01-14 17:28:29 -0800

    Finished Part 1

--- melons-abstract.py
+++ melons-abstract.py
@@ -7,10 +7,19 @@ class AbstractMelonOrder(object):
         self.species = species
         self.qty = qty
         self.shipped = False
+        self.get_total()
+        #self.mark_shipped()
 
     def get_total(self):
         """Calculate price."""
 
+        # to add flat fee:
+        # if order_type == "international" AND qty < 3:
+            #total += 3
+
+        #for christmas melons:
+        # if species == "Christmas":
+            # base_price *= 1.5
         base_price = 5
         total = (1 + self.tax) * self.qty * base_price
         return total
@@ -23,7 +32,6 @@ class AbstractMelonOrder(object):
 
 
 
-
 class DomesticMelonOrder(AbstractMelonOrder):
     """A concrete subclass of AbstractMelonOrder. Handles domestic orders."""
 
@@ -32,6 +40,8 @@ class DomesticMelonOrder(AbstractMelonOrder):
 
         self.order_type = "domestic"
         self.tax = 0.08
+
+        return super(DomesticMelonOrder, self).__init__(species, qty)
 
 
 
@@ -46,6 +56,8 @@ class InternationalMelonOrder(AbstractMelonOrder):
         self.order_type = "international"
         self.tax = 0.17
         self.country_code = country_code
+
+        return super(InternationalMelonOrder, self).__init__(species, qty)
 
     def get_country_code(self):
         """Return the country code."""
