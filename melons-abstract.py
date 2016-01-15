60f515a9e832992d0e323667a3b6f26f37a18bff
Hackbright Student <info@hackbrightacademy.com>
2016-01-14 17:57:12 -0800

    Cleaned up Part 2. Working on Part 3.

--- melons-abstract.py
+++ melons-abstract.py
@@ -1,26 +1,28 @@
 class AbstractMelonOrder(object):
     """Abstract class to handle domestic and international orders."""
 
-    def __init__(self, species, qty):
+    def __init__(self, species, qty, order_type, tax):
         """Initialize melon order attributes"""
 
         self.species = species
         self.qty = qty
         self.shipped = False
-        self.get_total()
-        #self.mark_shipped()
+        self.tax = tax
+        self.order_type = order_type
 
     def get_total(self):
         """Calculate price."""
 
         flat_fee = 3
         base_price = 5
+        total = 0
 
         # To increase price for Christmas melons:
         if self.species == "christmas" or self.species == "Christmas":
             base_price *= 1.5
 
-        total = (1 + self.tax) * self.qty * base_price
+
+        total = (1 + self.tax) * self.qty * base_price            
 
         # To add flat fee: 
         if self.order_type == "international" and self.qty < 10:
@@ -33,6 +35,14 @@ class AbstractMelonOrder(object):
 
         self.shipped = True
 
+class GovernmentMelonOrder(AbstractMelonOrder):
+    """A concrete subclass of AbstractMelonOrder that handles
+    US government order."""
+
+    pass
+    # self.tax = 0
+    # self.passed_inspection = False
+    # def inspection(self)
 
 
 
@@ -42,10 +52,7 @@ class DomesticMelonOrder(AbstractMelonOrder):
     def __init__(self, species, qty):
         """Initialize domestic melon order attributes"""
 
-        self.order_type = "domestic"
-        self.tax = 0.08
-
-        return super(DomesticMelonOrder, self).__init__(species, qty)
+        return super(DomesticMelonOrder, self).__init__(species, qty, "domestic", 0.08)
 
 
 
@@ -53,17 +60,17 @@ class DomesticMelonOrder(AbstractMelonOrder):
 class InternationalMelonOrder(AbstractMelonOrder):
     """A concrete subclass of AbstractMelonOrder. Handles international orders."""
 
+
     def __init__(self, species, qty, country_code):
         """Initialize international melon order attributes."""
 
-
-        self.order_type = "international"
-        self.tax = 0.17
         self.country_code = country_code
 
-        return super(InternationalMelonOrder, self).__init__(species, qty)
+        return super(InternationalMelonOrder, self).__init__(species, qty, "international", 0.17)
+
 
     def get_country_code(self):
         """Return the country code."""
 
         return self.country_code
+
